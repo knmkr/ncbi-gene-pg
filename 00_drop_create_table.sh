685bd54e62f4956f38bff3c6f90524bc834296de
Kensuke Numakura <knmkr3gma@gmail.com>
2017-03-24 18:52:01 +0900

    use db url

--- 00_drop_create_table.sh
+++ 00_drop_create_table.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+set -ue
 set -o pipefail
 
-PG_DB=$1
-PG_USER=$2
-BASE_DIR=$3
+PG_URL=$1
+BASE_DIR=$2
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <PG_DB> <PG_USER> <BASE_DIR>" >&2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <PG_URL> <BASE_DIR>" >&2
     exit 1
 fi
 
-echo "[contrib/gwascatalog] [INFO] Drop and Create tables..."
+echo "[INFO] Drop and Create tables..."
 
-psql $PG_DB $PG_USER --single-transaction -f ${BASE_DIR}/postgresql/schema/create_tables.sql -q
+psql ${PG_URL} --single-transaction -f ${BASE_DIR}/postgresql/schema/create_tables.sql -q
 
-echo "[contrib/gwascatalog] [INFO] Done"
+echo "[INFO] Done"
--- 01_import_data.sh
+++ 01_import_data.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-set -e
+set -ue
 set -o pipefail
 
-PG_DB=$1
-PG_USER=$2
-BASE_DIR=$3
-DATA_DIR=$4
+PG_URL=$1
+BASE_DIR=$2
+DATA_DIR=$3
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <PG_DB> <PG_USER> <BASE_DIR> <DATA_DIR>" >&2
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <PG_URL> <BASE_DIR> <DATA_DIR>" >&2
     exit 1
 fi
 
-echo "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetching data..."
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Fetching data..."
 
-DATA_NAME=NCBI_GENE
-mkdir -p ${DATA_DIR}/${DATA_NAME}
-cd ${DATA_DIR}/${DATA_NAME}
+mkdir -p ${DATA_DIR}/NCBI_GENE
+cd ${DATA_DIR}/NCBI_GENE
 
 # gene_info
 wget -c ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
@@ -27,18 +25,18 @@ wget -c ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
 # - GRCh37p13 (Annotation Release 105)
 wget -c ftp://ftp.ncbi.nlm.nih.gov/genomes/MapView/Homo_sapiens/sequence/ANNOTATION_RELEASE.105/initial_release/seq_gene.md.gz
 
-echo "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Importing data..."
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Importing data..."
 
 table=NcbiGeneInfo
 filename=gene_info.gz
 gzip -dc ${filename}| \
     awk -F$'\t' '{if ($1 == "9606") print }'| \
-    psql $PG_DB $PG_USER --no-psqlrc --single-transaction -c "TRUNCATE ${table}; COPY ${table} FROM stdin DELIMITERS '	'"
+    psql $PG_URL --no-psqlrc --single-transaction -c "TRUNCATE ${table}; COPY ${table} FROM stdin DELIMITERS '	'"
 
 table=NcbiSeqGene
 filename=seq_gene.md.gz
 gzip -dc ${filename}| \
     ${BASE_DIR}/script/cleanup_seqgene.py ${filename}| \
-    psql $PG_DB $PG_USER --no-psqlrc --single-transaction -c "TRUNCATE ${table}; COPY ${table} FROM stdin DELIMITERS '	'"
+    psql $PG_URL --no-psqlrc --single-transaction -c "TRUNCATE ${table}; COPY ${table} FROM stdin DELIMITERS '	'"
 
-echo "[contrib/gwascatalog] [INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
+echo "[INFO] `date +"%Y-%m-%d %H:%M:%S"` Done"
